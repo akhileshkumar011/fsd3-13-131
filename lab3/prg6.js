6ea8e303d9fb5628ed4dd8fa0e258c37073f7199
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-09-16 12:04:44 +0530

    add post request

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -5,9 +5,22 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.end("GET Request");
     } else if (req.url === "/" && req.method === "POST") {
-      res.statusCode = 200;
-      res.end("POST Request");
-    } else if (req.url === "/" && req.method === "PUT") {
+     let body = ''
+     req.on('data',(chunk)=>{
+      body += chunk;
+     });
+     req.on("end" , () => {
+      const product = JSON.parse(body);
+      console.log("received product :" , product);
+     });
+     
+     res.statusCode = 200;
+     res.end(JSON.stringify({msg:'product added', product}));
+
+    } 
+    
+    
+    else if (req.url === "/" && req.method === "PUT") {
       res.statusCode = 200;
       res.end("PUT Request");
     } else if (req.url === "/" && req.method === "DELETE") {
