46a7ddf03324ffb4fa99b1144d4dd65abddf2007
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-09-22 14:17:18 +0530

    getAllProducts

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,35 +1,54 @@
-import http from 'http'
-
+import http from "http";
+import { getAllProducts } from "./products.js";
 const server = http.createServer((req, res) => {
-    if (req.url === "/" && req.method === "GET") {
-      res.statusCode = 200;
-      res.end("GET Request");
-    } else if (req.url === "/" && req.method === "POST") {
-     let body = ''
-     req.on('data',(chunk)=>{
+  if (req.url === "/api/v1/products" && req.method === "GET") {
+    res.statusCode = 200;
+    const data = getAllProducts();
+    res.setHeader('content-type', 'application/json')
+    res.end(JSON.stringify({
+      count : data.length, 
+      data, 
+    })
+  ,);
+  } 
+  
+  else if (req.url === "/api/v1/products" && req.method === "POST") {
+    // console.log("Request:",req);
+    let body = "";
+    req.on("data", (chunk) => {
       body += chunk;
-     });
-     req.on("end" , () => {
+    });
+    req.on("end", () => {
       const product = JSON.parse(body);
-      console.log("received product :" , product);
-     });
-     
-     res.statusCode = 200;
-     res.end(JSON.stringify({msg:'product added', product}));
-
-    } 
-    
-    
-    else if (req.url === "/" && req.method === "PUT") {
+      console.log("received product:", product);
+      res.statusCode = 201;
+      res.end(JSON.stringify({ msg: "product added", product }));
+    });
+  } 
+  
+  else if (req.url.startsWith( "/products/") && req.method === "PUT") {
+    const productID = req.url.split('/').pop();
+    console.log('Update Product id:',productID);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const product = JSON.parse(body);
+      product.id = productID
       res.statusCode = 200;
-      res.end("PUT Request");
-    } else if (req.url === "/" && req.method === "DELETE") {
-      res.statusCode = 200;
-      res.end("DELETE Request");
-    } else {
-      res.statusCode = 404;
-      res.end("request not found");
-    }
-})
+      res.end(JSON.stringify({ msg: "product updated", product }));
+    });
+    
+  } 
+  
+  else if (req.url === "/" && req.method === "DELETE") {
+    res.statusCode = 200;
+    res.end("DELETE Request");
+  } else {
+    res.statusCode = 404;
+    res.end("request not found");
+  }
+});
 
-server.listen(5001, () => console.log("prg6 is running"));
+server.listen(5000, () => console.log("prg6 is running"));
