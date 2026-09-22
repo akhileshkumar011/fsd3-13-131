b70b038d899222d304924fd578b8d8dde6c30c1b
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-09-22 14:56:14 +0530

    lab3

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,5 +1,5 @@
 import http from "http";
-import { getAllProducts , addProduct } from "./products.js";
+import { getAllProducts , addProduct,deleteProduct } from "./products.js";
 const server = http.createServer((req, res) => {
   if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
       count : data.length, 
       data, 
     })
-  ,);
+  );
   } 
   
   else if (req.url === "/api/v1/products" && req.method === "POST") {
@@ -43,13 +43,20 @@ const server = http.createServer((req, res) => {
     
   } 
   
-  else if (req.url === "/" && req.method === "DELETE") {
+  else if (req.url.startsWith("/api/v1/products") && req.method === "DELETE") {
+    const pid = Number(req.url.split('/').pop());
+
     res.statusCode = 200;
-    res.end("DELETE Request");
+    if(deleteProduct(pid)){
+      res.end(JSON.stringify({msg:'item deleted '}));
+    }
+    else{
+      res.end(JSON.stringify({msg:`product with id ${pid} not found`}));
+    }
   } else {
     res.statusCode = 404;
     res.end("request not found");
   }
 });
 
-server.listen(5000, () => console.log("prg6 is running"));
+server.listen(5001, () => console.log("prg6 is running"));
--- lab3/products.js
+++ lab3/products.js
@@ -7,10 +7,20 @@ let nextId = 3;
 
 export const getAllProducts = () => {
     return products;
-}
+};
 export const addProduct = () =>{
     item.id = nextId;
     nextId++;
     products.push(item);
     return item;
-}
+};
+
+export const deleteProduct = (pid) => {
+    const item = products.findIndex((prd)=>prd.id === pid );
+    if(item == -1)
+        return false ;
+    products.splice(item,1)
+    console.log("products remaining :", products);
+    return true ;
+
+};
